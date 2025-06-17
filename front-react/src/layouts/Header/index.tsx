494554b493e5f647096ed831e0c43b013fda24a5
DESKTOP-28MJJB1\GIGABYTE <xogns6515@naver.com>
2025-06-17 21:30:28 +0900

    fix header

--- front-react/src/layouts/Header/index.tsx
+++ front-react/src/layouts/Header/index.tsx
@@ -1,17 +1,27 @@
 import React, { ChangeEvent, useRef, useState, KeyboardEvent, useEffect } from 'react'
 import './style.css'
-import { useNavigate, useParams } from 'react-router-dom'
-import { MAIN_PATH, SEARCH_PATH, USER_PATH, AUTH_PATH } from 'constant';
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { MAIN_PATH, SEARCH_PATH, USER_PATH, AUTH_PATH, BOARD_DETAIL_PATH, BOARD_WRITE_PATH, BOARD_PATH, BOARD_UPDATE_PATH } from 'constant';
 import { useCookies } from 'react-cookie';
-import { useLoginUserStore } from 'stores';
+import { useBoardStore, useLoginUserStore } from 'stores';
+import BaordDetail from 'views/Board/Detail';
 
 export default function Header() {
 
   const {loginUser,setLoginUser,resetLoginUser} = useLoginUserStore();
+  const {pathname} = useLocation();
   const [cookies, setCookie] = useCookies();
   const [isLogin,setLogin] = useState<boolean>(false);
-  const navigate = useNavigate();
+  const [isAuthPage,setAuthPage] = useState<boolean>(false);
+  const [isMainPage,setMainPage] = useState<boolean>(false);
+  const [isSearchPage,setSearchPage] = useState<boolean>(false);
+  const [isBoardDetailPage,setBoardDetailPage] = useState<boolean>(false);
+  const [isBoardWritePage,setBoardWritePage] = useState<boolean>(false);
+  const [isBoardUpdatePage,setBoardUpdatePage] = useState<boolean>(false);
+  const [isUserPage,setUserPage] = useState<boolean>(false);
 
+  
+  const navigate = useNavigate();
 
   const onLogoClickHandler = () => {
     navigate(MAIN_PATH());
@@ -95,9 +105,35 @@ export default function Header() {
     else{
       return <div className='black-button'>{'Login'}</div>
     }
-    
   };
 
+  const UploadButton = () => {
+    const {title,content,boardImageFileList,resetBoard} = useBoardStore(); 
+    const onSignInButtonClickHandler = () =>{
+
+    };
+    if(!title && content){
+      return <div className='black-button' onClick={onSignInButtonClickHandler}>{'Upload'}</div>
+    }
+    return <div className='disable-button'>{'Upload'}</div>
+  };
+  useEffect(() =>{
+    const isAuthPage = pathname.startsWith(AUTH_PATH());
+    setAuthPage(isAuthPage);
+    const isMainPage = pathname === MAIN_PATH();
+    setMainPage(isMainPage);
+    const isSearchPage = pathname.startsWith(SEARCH_PATH(''));
+    setSearchPage(isSearchPage);
+    const isBoardDetailPage = pathname.startsWith(BOARD_PATH()+'/'+BOARD_DETAIL_PATH(''));
+    setBoardDetailPage(isBoardDetailPage);
+    const isBoardWritePage = pathname.startsWith(BOARD_PATH()+'/'+BOARD_WRITE_PATH());
+    setBoardWritePage(isBoardWritePage);
+    const isBoardUpdatePage = pathname.startsWith(BOARD_PATH()+'/'+BOARD_UPDATE_PATH(''));
+    setBoardUpdatePage(isBoardUpdatePage);
+    const isUserPage = pathname.startsWith(USER_PATH(''));
+    setUserPage(isUserPage);
+
+  },[pathname]);
   return(
     <div id='header'>
       <div className='header-container'>
@@ -108,10 +144,12 @@ export default function Header() {
           <div className='header-logo'>{'test board'}</div> 
         </div>
         <div className='header-right-box'>
-          <SearchButton/>  
-          <MyPageButton/>
+          {(isAuthPage || isMainPage || isSearchPage || isBoardDetailPage) && <SearchButton/>} 
+          {(isMainPage || isSearchPage || isBoardDetailPage || isUserPage) && <MyPageButton/>}
+          {(isBoardWritePage || isBoardUpdatePage) && <UploadButton/>}
         </div> 
       </div>
     </div>
   );
 }
+
