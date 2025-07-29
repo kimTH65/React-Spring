1a6ff4d783e43c079736ab64fafa83ebc99ff17b
DESKTOP-28MJJB1\GIGABYTE <xogns6515@naver.com>
2025-07-29 19:50:25 +0900

    craete login box(front)

--- front-react/src/components/InputBox/index.tsx
+++ front-react/src/components/InputBox/index.tsx
@@ -9,7 +9,7 @@ interface Props {
     setValue: React.Dispatch<React.SetStateAction<string>>;
     error: boolean;
 
-    icon?: string;
+    icon?: 'eye-light-off-icon' | 'eye-light-on-icon' | 'expand-right-light-icon';
     onButtonClick?: () => void;
 
     message?: string;
@@ -39,7 +39,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
             <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
                 <input ref={ref} type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} onKeyDown={onKeyDownHanlder}/>
                 {onButtonClick !== undefined && (
-                    <div className='icon-button'>
+                    <div className='icon-button' onClick={onButtonClick}>
                         {icon !== undefined && (<div className ={`icon ${icon}`}></div>)}
                     </div>
                 )}
--- front-react/src/views/Authentication/index.tsx
+++ front-react/src/views/Authentication/index.tsx
@@ -1,16 +1,77 @@
-import React, { useState } from 'react'
+import React, { useState,KeyboardEvent, useRef } from 'react'
 import './style.css'
+import InputBox from 'components/InputBox';
 
 export default function Authentication() {
 
     const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
     
     const SignInCard = () => {
+      const emailRef = useRef<HTMLInputElement | null>(null);
 
-        return (
-            <div className='auth-card'></div>
-        );
-    }
+      const passwordRef = useRef<HTMLInputElement | null>(null);
+      // state: email
+      const [email, setEmail] = useState<string>('');
+
+      // state: password
+      const [password, setPassword] = useState<string>('');
+
+      // state: password input type
+      const [passwordType, setPasswordType] = useState<'text' | 'password'>('password');
+
+      // state: error status
+      const [error, setError] = useState<boolean>(false);
+      
+      // state: password button icon state
+      const [passwordButtonIcon, setPasswordButtonIcon] = useState<'eye-light-off-icon' | 'eye-light-on-icon'>('eye-light-off-icon');
+
+      const onPasswordButtonClickHandler = () => {
+        if (passwordType ==='text'){
+          setPasswordType('password');
+          setPasswordButtonIcon('eye-light-off-icon')
+        }else{
+          setPasswordType('text');
+          setPasswordButtonIcon('eye-light-on-icon')
+        }
+      } 
+      const onSignInButtonClickHandler = () => {
+
+      }
+      const onEmailKeyDownHandler = (event : KeyboardEvent<HTMLInputElement>) => {
+        if (event.key !=='Enter')return;
+        if (!passwordRef.current)return;
+        passwordRef.current.focus();
+      } 
+      const onPasswordKeyDownHandler = (event : KeyboardEvent<HTMLInputElement>) => {
+        if (event.key !=='Enter')return;
+        onSignInButtonClickHandler();
+      } 
+      
+      return (
+        <div className='auth-card'>
+          <div className='auth-card-box'>
+            <div className='auth-card-top'>
+              <div className='auth-card-title-box'>
+                <div className='auth-card-title'>{'Login'}</div>
+              </div>
+              <InputBox ref={emailRef} label='Email Address' type='text' placeholder='Please enter your email address.' error={error} value={email} setValue={setEmail} onKeyDown={onEmailKeyDownHandler}/>
+              <InputBox ref={passwordRef} label='Password' type={passwordType} placeholder='Please enter your password.' error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
+            </div>
+            <div className='auth-card-bottom'>
+              <div className='auth-sign-in-error-box'>
+                <div className='auth-sign-in-error-message'>
+                  {'The email address or password you entered is incorrect.\nPlease check your input and try again.'}
+                </div>
+              </div>
+              <div className='black-large-full-button' onClick={onSignInButtonClickHandler}>{'Login'}</div>
+              <div className='auth-description-box'>
+                <div className='auth-description'>{'New user?'}<span className='auth-description-link'>{'Sign up here'}</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    };
 
     const SignUpCard = () => {
         return(
